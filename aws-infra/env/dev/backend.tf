f40432faa0d92710e36ee6cd67e9a043d7c287ac
henry-lam-datllc <henry.lam@datllc.tech>
2025-04-14 21:26:04 +0700

    fix lint

--- aws-infra/env/dev/backend.tf
+++ aws-infra/env/dev/backend.tf
@@ -9,3 +9,14 @@ terraform {
     region         = "ap-southeast-1"             # Specify the AWS region for the S3 bucket
   }
 }
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+  }
+}
--- aws-infra/env/dev/main.tf
+++ aws-infra/env/dev/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+  }
+}
 # Start Generation Here
 module "ec2_instance" {
 
